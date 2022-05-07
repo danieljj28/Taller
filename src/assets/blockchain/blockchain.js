b3f1dde2575670eb52bc52e263df4a95fda61a5f
xabiersanchez <xabaco890@gmail.com>
2022-05-07 16:37:43 +0200

    primer mergeo

--- src/assets/blockchain/blockchain.js
+++ src/assets/blockchain/blockchain.js
@@ -15,25 +15,18 @@ export class Bloque {
     this.transacciones = transacciones;
     this.hashBloqueAnterior = hashBloqueAnterior;
     this.hash = this.calcularHash();
-    this.numeroParaValidacion = 0;
   }
 
   calcularHash() {
     return SHA256(
         this.hashBloqueAnterior +
         this.fecha +
-        JSON.stringify(this.transacciones) +
-        this.numeroParaValidacion
+        JSON.stringify(this.transacciones)
     ).toString();
   }
 
   minarBloque() {
-    while (
-      this.hash.substring(0, 1) !== Array(1 + 1).join("0")
-    ) {
-      this.numeroParaValidacion++;
-      this.hash = this.calcularHash();
-    }
+    this.hash = this.calcularHash();
   }
 }
 
@@ -85,54 +78,6 @@ export class BlockchainVehiculos {
     return true;
   }
 }
-
-let motorBlockchain = new BlockchainVehiculos();
-
-console.log("Minado para la validacion de los bloques de motorBlockchain en proceso...");
-motorBlockchain.anyadirNuevoBloque(
-  new Bloque("01/06/2020", {
-    vehiculo: "9394GCJ",
-    taller: "Taller Kike",
-    tipoConsulta: "Revision",
-    detalles: "Cambio pastillas de freno"
-  })
-);
-
-motorBlockchain.anyadirNuevoBloque(
-    new Bloque("01/06/2020", {
-        vehiculo: "1234BBB",
-        taller: "Taller Audi Alicante",
-        tipoConsulta: "Reparacion",
-        detalles: "Cambio chasis frontal"
-    })
-);
-
-motorBlockchain.anyadirNuevoBloque(
-    new Bloque("01/06/2020", [{
-        vehiculo: "0000QWR",
-        taller: "Taller Mercedes Madrid",
-        tipoConsulta: "Reparacion",
-        detalles: "Cambio tubo de escape"
-    },
-    {
-        vehiculo: "1111PPP",
-        taller: "Taller Mercedes Madrid",
-        tipoConsulta: "Revision",
-        detalles: "ITV"
-    }])
-);
-
-motorBlockchain.crearTransaccion(new Transaccion('1234QQQ', 'Taller Jaime', 'Reparacion', 'Motor inexistente'));
-motorBlockchain.crearTransaccion(new Transaccion('1234QZQ', 'Taller Perez', 'Reparacion', 'Motor inexistente'));
-motorBlockchain.crearTransaccion(new Transaccion('1234QQD', 'Taller Juan', 'Reparacion', 'Motor inexistente'));
-motorBlockchain.minarTransaccionesPendientes();
-
-motorBlockchain.crearTransaccion(new Transaccion('1234ZZZ', 'Taller Jaime', 'Reparacion', 'Motor inexistente'));
-motorBlockchain.crearTransaccion(new Transaccion('1234PPP', 'Taller Perez', 'Reparacion', 'Motor inexistente'));
-motorBlockchain.crearTransaccion(new Transaccion('1234TTT', 'Taller Juan', 'Reparacion', 'Motor inexistente'));
-motorBlockchain.minarTransaccionesPendientes();
-
-console.log(JSON.stringify(motorBlockchain, null, 4));
 
 /*
 const fs = require('fs')
