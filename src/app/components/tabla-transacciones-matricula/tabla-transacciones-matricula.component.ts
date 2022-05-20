abe153e4f9e34a2b658911e96f75b4c2085a3d51
Daniel Jaen <danijj28@gmail.com>
2022-05-20 12:10:40 +0200

    pdf

--- src/app/components/tabla-transacciones-matricula/tabla-transacciones-matricula.component.ts
+++ src/app/components/tabla-transacciones-matricula/tabla-transacciones-matricula.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-tabla-transacciones-matricula',
@@ -8,7 +10,53 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TablaTransaccionesMatriculaComponent implements OnInit {
   @Input() public transacciones:any;
 
-  constructor() { }
+  VIDEOGAMES = [
+    {
+      id : 1,
+      name: "Animal Crossing",
+      platform: "Nintendo Switch",
+      reference : "1-770-736-8031"
+    },
+    {
+      id: 2,
+      name: "The Legend of Zelda: Ocarina of Time CV",
+      platform: "Wii U",
+      reference: "1-770-736-2323"
+    },
+    {
+      id: 3,
+      name: "Metal Gear Solid",
+      platform: "Playstation (PSX)",
+      reference: "1-4564-736-334"
+    },
+    {
+      id: 4,
+      name: "ShenMue",
+      platform: "Sega Dreamcast",
+      reference: "3-770-736-4532"
+    },
+    {
+      id: 5,
+      name: "Rise of the Tomb Raider",
+      platform: "Playstation 4",
+      reference: "1-324-736-3245"
+    },
+    {
+      id: 6,
+      name: "Resident Evil 2",
+      platform: "Playstation",
+      reference: "1-123-3336-4321"
+    }
+  ];
+  
+  constructor() {
+  }
+  public downloadPDF(): void {
+    const doc = new jsPDF();
+
+    doc.text('Hello world!', 10, 10);
+    doc.save('hello-world.pdf');
+  }
 
   ngOnInit(): void {
   }
