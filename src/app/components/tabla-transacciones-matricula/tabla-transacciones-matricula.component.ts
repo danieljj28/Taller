a0c4ca1c9b4c2bfb3b70a9264fb7c86b2cc5f368
Daniel Jaen <danijj28@gmail.com>
2022-05-20 13:55:54 +0200

    formato pdf

--- src/app/components/tabla-transacciones-matricula/tabla-transacciones-matricula.component.ts
+++ src/app/components/tabla-transacciones-matricula/tabla-transacciones-matricula.component.ts
@@ -9,53 +9,41 @@ import html2canvas from 'html2canvas';
 })
 export class TablaTransaccionesMatriculaComponent implements OnInit {
   @Input() public transacciones:any;
-
-  VIDEOGAMES = [
-    {
-      id : 1,
-      name: "Animal Crossing",
-      platform: "Nintendo Switch",
-      reference : "1-770-736-8031"
-    },
-    {
-      id: 2,
-      name: "The Legend of Zelda: Ocarina of Time CV",
-      platform: "Wii U",
-      reference: "1-770-736-2323"
-    },
-    {
-      id: 3,
-      name: "Metal Gear Solid",
-      platform: "Playstation (PSX)",
-      reference: "1-4564-736-334"
-    },
-    {
-      id: 4,
-      name: "ShenMue",
-      platform: "Sega Dreamcast",
-      reference: "3-770-736-4532"
-    },
-    {
-      id: 5,
-      name: "Rise of the Tomb Raider",
-      platform: "Playstation 4",
-      reference: "1-324-736-3245"
-    },
-    {
-      id: 6,
-      name: "Resident Evil 2",
-      platform: "Playstation",
-      reference: "1-123-3336-4321"
-    }
-  ];
   
   constructor() {
   }
-  public downloadPDF(): void {
-    const doc = new jsPDF();
 
-    doc.text('Hello world!', 10, 10);
-    doc.save('hello-world.pdf');
+  public downloadPDF(): void {
+    const DATA: any = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+  }
+
+  public showDiv(): void {
+    const name = document.getElementById('htmlData')
+    // Forzamos la variable a boolean
+    // Si name existe y no es un string vacío, entonces devuelve true
+    name?.hidden;
   }
 
   ngOnInit(): void {
