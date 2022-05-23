656ed4470cb1ac81db1a178aa8b37f7fad2349ca
Daniel Jaen <danijj28@gmail.com>
2022-05-23 12:47:15 +0200

    siii

--- src/app/components/tabla-transacciones-matricula/tabla-transacciones-matricula.component.ts
+++ src/app/components/tabla-transacciones-matricula/tabla-transacciones-matricula.component.ts
@@ -12,49 +12,17 @@ export class TablaTransaccionesMatriculaComponent implements OnInit {
   @Input() public transacciones:any;
 
 
-  /*public downloadPDF() : void{
-    // Extraemos el
-
+  public downloadPDF(): void {
     const DATA: any = document.getElementById('htmlData');
-    DATA.style.display = "block"
-    const doc = new jsPDF('p', 'pt', 'a4');
-    const options = {
-      background: 'white',
-      scale: 3
-
-    };
-    DATA.style.display = "none";
-    html2canvas(DATA, options).then((canvas) => {
-
-      const img = canvas.toDataURL('image/PNG');
-
-      // Add image Canvas to PDF
-      const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
-    }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
-    });
-  }*/
-
-
-  constructor() {
-    this.downloadPDF();
-  }
-  downloadPDF() {
-    // Extraemos el
-
-    let DATA : any = document.getElementById('htmlData');
     DATA.style.display = "block";
     const doc = new jsPDF('p', 'pt', 'a4');
+
     const options = {
       background: 'white',
       scale: 3
     };
+
+
     html2canvas(DATA, options).then((canvas) => {
 
       const img = canvas.toDataURL('image/PNG');
@@ -68,8 +36,13 @@ export class TablaTransaccionesMatriculaComponent implements OnInit {
       doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
       return doc;
     }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      docResult.save("registros");
     });
+
+    DATA.style.display = "none";
+
+  }
+  constructor() {
   }
 
   ngOnInit(): void {
--- src/assets/blockchain/generakeys.js
+++ src/assets/blockchain/generakeys.js
@@ -1,16 +1,13 @@
 const EC = require('elliptic').ec;
 
-// You can use any elliptic curve you want
 const ec = new EC('secp256k1');
 
-// Generate a new key pair and convert them to hex-strings
 const key = ec.genKeyPair();
 const publicKey = key.getPublic('hex');
 const privateKey = key.getPrivate('hex');
 
-// Print the keys to the console
 console.log();
-console.log('Your public key (also your wallet address, freely shareable)\n', publicKey);
+console.log('tu clave publica es (direccion de tu waller)\n', publicKey);
 
 console.log();
-console.log('Your private key (keep this secret! To sign transactions)\n', privateKey);
+console.log('Tu clave privada (Mantela secreta muy importante)\n', privateKey);
